4cac2cc377712633e4c7e3d32b716f0c83467061
kongyi <kywj1997@gmail.com>
2021-03-22 10:36:44 +0800

    使用hash路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
   return (
-     <div>Hello</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tags">
+            <Tag />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+function Statistics() {
+  return <h2>统计</h2>;
+}
+
+function Tag() {
+  return <h2>标签</h2>;
+}
+
+function Money() {
+  return <h2>记账</h2>;
+}
+function NoMatch() {
+  return (
+    <div>该页面不存在</div>
+  )
+}
+
 
 export default App;
