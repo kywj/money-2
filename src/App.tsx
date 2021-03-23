cb928ca035e640e079c17d2691e62c30cd7b8100
kongyi <kywj1997@gmail.com>
2021-03-23 11:10:28 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,56 +5,52 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-import styled from "styled-components";
-import Nav from "components/Nav";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from "components/Layout";
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tag />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from="/" to="/money" />
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tag />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/" to="/money" />
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 function Statistics() {
-  return <h2>统计</h2>;
+  return (
+    <Layout>
+      <h2>统计</h2>
+    </Layout>
+  );
 }
 
 function Tag() {
-  return <h2>标签</h2>;
+  return (
+    <Layout>
+      <h2>标签</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账</h2>;
+  return (
+    <Layout>
+      <h2>记账</h2>
+    </Layout>
+  );
 }
 function NoMatch() {
   return <div>该页面不存在</div>;
