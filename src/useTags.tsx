063812b3441c9b07dad74713a7480a3f248644cd
kongyi <kywj1997@gmail.com>
2021-03-31 12:07:56 +0800

    修复tag Id

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,13 +1,15 @@
 import { createId } from "lib/createId";
 import { useState } from "react";
 
+const defaultTags = [
+  { id: createId(), name: "衣" },
+  { id: createId(), name: "食" },
+  { id: createId(), name: "住" },
+  { id: createId(), name: "行" },
+];
+
 function useTags() {
-  const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    { id: createId(), name: "衣" },
-    { id: createId(), name: "食" },
-    { id: createId(), name: "住" },
-    { id: createId(), name: "行" },
-  ]);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   return { tags, setTags };
 }
 
