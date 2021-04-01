178b31e47654e799156ddd2c46b32d968cb14d42
kongyi <kywj1997@gmail.com>
2021-04-01 11:37:21 +0800

    创建 Input组件

--- src/views/money/NoteSection.tsx
+++ src/views/money/NoteSection.tsx
@@ -1,25 +1,11 @@
-import React from "react";
+import { Input } from "components/Input";
+import React, { ChangeEventHandler } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label {
-    display: flex;
-    align-items: center;
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -29,17 +15,17 @@ type Props = {
 
 const NoteSection: React.FC<Props> = (props) => {
   const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
+  };
   return (
     <Wrapper>
       <label>
-        <span>备注</span>
-        <input
-          type="text"
+        <Input
+          label="备注"
           placeholder="请输入备注"
           value={note}
-          onChange={(e) => {
-            props.onChange(e.target.value);
-          }}
+          onChange={onChange}
         />
       </label>
     </Wrapper>
