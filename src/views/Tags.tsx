bcf71b5d6b28fc040c77a143bb1e9adc46797ad2
kongyi <kywj1997@gmail.com>
2021-04-01 11:24:04 +0800

    创建 Button 组件

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -46,9 +46,7 @@ function Tag() {
         {tags.map((tag) => (
           <li key={tag.id}>
             <Link to={"/tags/" + tag.id}>
-              <span className="oneLine">
-                {tag.id}:{tag.name}
-              </span>
+              <span className="oneLine">{tag.name}</span>
               <Icon name="right" />
             </Link>
           </li>
